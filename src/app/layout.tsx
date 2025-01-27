451b68adf58abba4ed87cf0b335a49b1bb5a55e2
Prydeton <maxwellreid.tech@gmail.com>
2025-01-27 16:11:17 +1100

    Add project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import '@/app/styles/globals.css'
+import { bodyFont, headingFont } from '@/app/styles/fonts'
+
+export const metadata: Metadata = {
+  title: 'Matcha Meter',
+  description: '[TODO]',
+  keywords: ['TODO'],
+}
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) => {
+  return (
+    <html lang="en" className={`${headingFont.variable} ${bodyFont.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <>Matcha Meter - Coming soon</>
+}
+
+export default Home
