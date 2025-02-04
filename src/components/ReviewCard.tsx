82714482c7d0813cd69ffb41f5e58d3e0097d8da
Prydeton <maxwellreid.tech@gmail.com>
2025-02-04 22:25:06 +1100

    Display leafs for review rating

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 
 const Card = ({ name, city, suburb, imageSrc, drink, price, rating, hexCode, googleUrl }: Review) => {
   return (
-    <div className="bg-[#CAD5C8] h-24">
+    <div className="bg-[var(--surface)]">
       <div style={{ '--bg-color': `#${hexCode}` } as React.CSSProperties} className="bg-[var(--bg-color)] h-5" />
       <div className="flex">
-        <Image src={imageSrc} alt={`A drink from ${name}`} width={80} height={80} />
-        <div>
+        <Image src={imageSrc} alt={`A drink from ${name}`} width={3} height={4} />
+        <div className="m-2">
           <h2>
             <a href={googleUrl}>{name}</a>
           </h2>
@@ -16,7 +16,19 @@ const Card = ({ name, city, suburb, imageSrc, drink, price, rating, hexCode, goo
           <p>
             {drink} - ${price}
           </p>
-          <div>{rating}</div>
+          <div className="flex">
+            {Array.from({ length: Math.floor(rating) }, (_, i) => (
+              <img key={i} src="/leaf.svg" alt="Star" className="w-6 h-6" />
+            ))}
+            {rating % 1 > 0 && (
+              <img
+                style={{ '--scale': rating % 1 } as React.CSSProperties}
+                src="/leaf.svg"
+                alt="Star"
+                className="w-6 h-6 scale-[var(--scale)]"
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
